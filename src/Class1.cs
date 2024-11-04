1d184e5b6e410e08c54a3461e15e4c25d63c85e5
Георгий Возмилов <georgpv@mail.ru>
2024-11-04 17:23:03 +0300

    фывфыв

---
+++ src/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckGame.Magic_Wand
+{
+    public static class QuackUtility
+    {
+        public static void DoQuack(ref Duck duck)
+        {
+            if (duck.dead || duck.inputProfile == null || Level.current == null)
+                return;
+            float leftTrigger = duck.inputProfile.leftTrigger;
+            if (duck.inputProfile.hasMotionAxis)
+                leftTrigger += duck.inputProfile.motionAxis;
+            var equipment = duck.GetEquipment(typeof(Hat));
+                if (Network.isActive)
+                    duck._netQuack.Play(pit: leftTrigger);
+                else if (equipment != null && ((Hat) equipment).quacks)
+                    ((Hat) equipment).Quack(1f, leftTrigger);
+                else 
+                    duck._netQuack.Play(pit: leftTrigger);
+            if (duck.isServerForObject)
+                ++Global.data.quacks.valueInt;
+            ++duck.profile.stats.quacks;
+            duck.quack = 20;
+        }
+    }
+}
--- src/Stand.cs
+++ src/Stand.cs
@@ -48,9 +48,9 @@ namespace DuckGame.Magic_Wand
             {
                 
             }
+            Duck duck = null;
             if(obj is Duck && ((Duck)obj) != this.own)
             {
-                //((Duck)obj)._destroyed = true;
                 if (this.x <= obj.x)
                 {
                     obj.ApplyForce(new Vec2(1f, -0.7f));
@@ -59,6 +59,7 @@ namespace DuckGame.Magic_Wand
                 {
                     obj.ApplyForce(new Vec2(-1f, -0.7f));
                 }
+                duck = (Duck)obj;
                 ((Duck)obj).GoRagdoll();
             }
             if(obj is RagdollPart)
@@ -71,6 +72,11 @@ namespace DuckGame.Magic_Wand
                 {
                     obj.ApplyForce(new Vec2(-1f, -0.7f));
                 }
+                duck = ((RagdollPart)obj).duck;
+            }
+            if (duck != null)
+            {
+                QuackUtility.DoQuack(ref duck);
             }
         }
 
